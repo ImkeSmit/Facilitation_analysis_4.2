5398712fa40da8a08122271467fe40cf7c61708e
ImkeSmit <imke610@gmail.com>
2024-01-23 11:35:02 +0200

    Data wrangling the quadrat data

--- Scripts/Trait name changing.R
+++ Scripts/Trait name changing.R
@@ -2,6 +2,7 @@
 #Let's fix them
 library(tidyverse)
 library(tidylog)
+library(readxl)
 
 
 ###read in the facilitation data for each country
@@ -59,4 +60,90 @@ write_csv2(fac_names_total, "C:\\Users\\imke6\\Documents\\Msc Projek\\Functional
 
 write_csv2(trait_names, "C:\\Users\\imke6\\Documents\\Msc Projek\\Functional trait analysis\\Functional trait data\\trait_names.csv") 
 
+
+###Let's look at the state of the names in the quadrat data###
+#The quadrat data has a sheet for every country, lets extract only the countries for which we have facilitation data
+# Specify the file path to your Excel file
+excel_file <- "C:\\Users\\imke6\\Documents\\Msc Projek\\Functional trait analysis\\Functional trait data\\Cover quadrats_98sites_16_12_19.xlsx"
+
+# Specify the sheets you want to extract
+fac_countries <- c("Algeria", "Argentina", "Australia", "Chile", "China", "Iran", 
+                                     "Israel", "Namibia", "South Africa",  "Spain")
+
+# Create a list to store the data frames for each selected sheet
+selected_sheets_data <- list()
+
+# Loop through each sheet and extract the data
+for (sheet_name in fac_countries) {
+  sheet_data <- read_excel(excel_file, sheet = sheet_name)
+  selected_sheets_data[[sheet_name]] <- sheet_data
+}# Now, selected_sheets_data contains data frames for the fac countries
+length(selected_sheets_data)
+names(selected_sheets_data)
+
+
+
+##Each country's sheet contains multiple species x site matrices, one for each plot.
+#each matrix is separated by a row of NA's. We need to cut the dataframe at each row of NA's 
+# Function to cut dataframe at each row with NA in the first column
+cut_dataframe <- function(df) {
+  # Find the row indices where the first column is NA
+  cut_indices <- which(is.na(df[ , 1]))
+  
+  # Split the dataframe at the identified indices
+  split_dataframes <- split(df, cumsum(seq_along(df[ , 1]) %in% cut_indices))
+  
+  # Remove empty dataframes (resulting from consecutive NA rows)
+  split_dataframes <- split_dataframes[sapply(split_dataframes, function(x) !all(is.na(x[ , 1])))]
+  
+  return(split_dataframes)
+}
+
+##Now cut the dataframes of each country
+for (i in 1:length(selected_sheets_data)) {
+  country <- as.data.frame(selected_sheets_data[[i]]) #select one country from the list
+  country <- rbind(colnames(country), country) #change the column names to the first row
+  colnames(country) <- c("Plotname", seq(1:(ncol(country)-1))) #give new column names
+  if (i == 1) {
+  country_cut <- cut_dataframe(country)
+  } else {
+    temp_country_cut <- cut_dataframe(country)
+    country_cut <- c(country_cut, temp_country_cut)
+  }
+} #country_cut is a list of dataframes. each dataframe is a species x site matrix of a plot. It contains the matrices of all countries
+
+
+###Now we need to extract the plots that are also in the facilitation data
+##read in the facilitation data for each country
+wd <- "C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv2"
+data_files <- list.files(wd)
+countrynames <- c("algeria", "argentina", "australia", "chile", "chinachong", "chinaxin", "iranabedi", "iranfarzam", 
+                  "israel", "namibiablaum", "namibiawang", "southafrica",  "spainmaestre", "spainrey")
+for(i in 1:length(data_files)) {                              
+  assign(paste0(countrynames[i]),                                   
+         read.csv2(paste0("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv2\\",
+                          data_files[i])))
+}
+
+#get the site names from the facilitation data
+for (k in 1:length(countrynames)) {
+  cou <- get(countrynames[k])
+  if (k == 1) {
+    fac_sitenames <- c(unique(cou$SITE))
+  } else {
+    temp_sitenames <- c(unique(cou$SITE))
+    fac_sitenames <- c(fac_sitenames, temp_sitenames)
+  }
+}
+
+##Now subset
+country_cut_copy <- country_cut
+
+for (n in 1:length(country_cut_copy)) {
+  plot <- country_cut_copy[[n]]
+  
+  if(plot[1,1] %like% fac_sitenames == FALSE) {
+    country_cut_copy[[n]] < NA
+    } 
+  }
 
