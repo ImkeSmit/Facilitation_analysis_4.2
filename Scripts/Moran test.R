065be24b9388b973b50ad39e3e382cff1854c1de
ImkeSmit <imke610@gmail.com>
2024-04-05 14:28:08 +0200

    make nint result a shapefile

--- Scripts/Moran test.R
+++ Scripts/Moran test.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(tidylog)
 library(sf)
+library(ggplot2)
 
 
 #import nint results
@@ -12,6 +13,12 @@ siteinfo <- read.csv("Facilitation data//BIODESERT_sites_information.csv") |>
   select(ID, Lat_decimal, Long_decimal)
 
 #join the coordinates to nint result
-nint_result <- nint_result |> 
-  left_join(siteinfo, by = "ID")
+nint_result_join <- nint_result |> 
+  left_join(siteinfo, by = "ID") |> 
+  select(ID, NIntc_richness, Lat_decimal, Long_decimal)
 
+#make the table a shapefile"
+nint_result_shp <- st_as_sf(nint_result_join, coords = c(3,4))
+#plot it:
+ggplot() +
+  geom_sf(data = nint_result_shp, aes(color = NIntc_richness))
