d3d93c823cd9ed3de564d6b1a5188c79bfc9aae9
ImkeSmit <imke610@gmail.com>
2024-04-05 13:38:22 +0200

    import data and join spatial data

--- Scripts/Moran test.R
+++ Scripts/Moran test.R
@@ -1,1 +1,17 @@
 ##This script is to check if the facilitation data is spatially autocorrelated
+library(tidyverse)
+library(tidylog)
+library(sf)
+
+
+#import nint results
+nint_result <- read.csv("Facilitation data//results//NIntc_results_allcountries_6Feb2024.csv", row.names = 1)
+
+#import the siteinfo, which has coordinates for each site
+siteinfo <- read.csv("Facilitation data//BIODESERT_sites_information.csv") |> 
+  select(ID, Lat_decimal, Long_decimal)
+
+#join the coordinates to nint result
+nint_result <- nint_result |> 
+  left_join(siteinfo, by = "ID")
+
