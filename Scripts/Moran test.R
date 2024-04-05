f1f285a136594bce49e4f447737d960ce519e115
ImkeSmit <imke610@gmail.com>
2024-04-05 15:05:09 +0200

    use great circle distances

--- Scripts/Moran test.R
+++ Scripts/Moran test.R
@@ -2,9 +2,8 @@
 #we will do the moran test at the plot level. Thus we need to summarise the nintc at the plot level
 library(tidyverse)
 library(tidylog)
-library(sf)
-library(ggplot2)
 library(ape)
+library(geosphere)
 
 
 #import nint results
@@ -25,8 +24,11 @@ nint_result_join <- nint_result |>
   select(!c(NIntc_richness, NIntc_cover, NIntc_shannon, NInta_cover, NInta_richness, NInta_shannon, replicate_no, nurse))
 
 #generate a matrix with the inverse distance between each coordinate
-#matrix of euclidean distances
-distmat <- as.matrix(dist(cbind(nint_result_join$Long_decimal, nint_result_join$Lat_decimal)))
+#matrix of distances between points
+#use distGeo because points are spread accross the world, need to take curvature of earth into account
+distmat <- 
+  as.matrix(distm(nint_result_join[, c(which(colnames(nint_result_join) == "Long_decimal"), which(colnames(nint_result_join) == "Lat_decimal"))], 
+        fun = distGeo))
 #get the inverse:
 distmat_inv <- 1/distmat
 diag(distmat_inv) <- 0
