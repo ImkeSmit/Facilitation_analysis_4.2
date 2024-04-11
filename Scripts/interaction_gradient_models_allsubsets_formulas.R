817406e984d226e5168072ebf9e3472a5a1d4771
ImkeSmit <imke610@gmail.com>
2024-04-11 10:12:21 +0200

    add code to get species preference along aridity

--- Scripts/interaction_gradient_models_allsubsets_formulas.R
+++ Scripts/interaction_gradient_models_allsubsets_formulas.R
@@ -180,6 +180,127 @@ for(r in 1:length(response_list)) {
 ##if there is no AIC value, the model did not converge
 results_table
 
+
+###Species preference along aridity####
+#Does aridity influence how many species grow exclusively in bare, open and both microsites (Pbare and Pdominant analysis)
+#We require raw country data
+data_files <- list.files("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3")
+countrynames <- c("algeria", "argentina", "australia", "chile", "chinachong", "chinaxin", "iranabedi", "iranfarzam", 
+                  "israel", "namibiablaum", "namibiawang", "southafrica",  "spainmaestre", "spainrey")
+for(i in 1:length(data_files)) {                              
+  assign(paste0(countrynames[i]),                                   
+         read.csv2(paste0("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3\\",
+                          data_files[i])))
+}
+
+##This is a PLOTLEVEL analysis
+##Calculate the number of species that prefer each microsite in each plot of each country
+countrynames <- c("algeria", "argentina", "australia", "chile", "chinachong", "chinaxin", "iranabedi", "iranfarzam", 
+                  "israel", "namibiablaum", "namibiawang", "southafrica",  "spainmaestre", "spainrey")
+
+preflist <- c("pref_algeria", "pref_argentina", "pref_australia", "pref_chile", "pref_chinachong", "pref_chinaxin", 
+              "pref_iranabedi", "pref_iranfarzam", "pref_israel", "pref_namibiablaum", "pref_namibiawang", 
+              "pref_southafrica",  "pref_spainmaestre", "pref_spainrey")
+
+column_names <- c("site_ID", "ID", "aridity", "graz", "prop_bare_only", "prop_nurse_only", "prop_both")
+
+
+for (i in 1:length(countrynames)) {
+  cou <- get(countrynames[i]) #working with one country at a time
+  assign(paste("pref", countrynames[i], sep= "_"), #create an empty pref table that will have the values for each site in a country
+         data.frame(matrix(nrow = length(unique(as.factor(cou$ID))), ncol = length(column_names))))
+  pref_cou <- (get(preflist[i])) #assign this empty table to result_cou
+  colnames(pref_cou) = column_names
+  
+  plotlist <- c(unique(cou$ID))
+  l = 1
+  
+  for (f in 1:length(plotlist)) {
+    plot <- cou[which(cou$ID == plotlist[f]) , ] #select a plot in a country
+    plot <- plot[which(!is.na(plot$Species.within.quadrat)) , ] #remove rows that had no species in a microsite
+    
+    all_sp <- unique(plot$Species.within.quadrat) #all species at the plot
+    
+    bare <- plot[which(plot$Microsite == 1) , ] #select only bare microsites
+    bare_sp <- unique(bare$Species.within.quadrat) #species in bare microsites
+    
+    nurse <- plot[which(plot$Microsite == 2) , ] #select only nurse microsites
+    nurse_sp <- unique(nurse$Species.within.quadrat) #species in nurse microsites
+    
+    bare_match <- match(nurse_sp, bare_sp) #the indexes in bare_sp that correspond to the names in nurse_sp
+    #eg the second name in nurse_sp corresponds to the 4th name in bare sp. Thus the match is 4
+    
+    nurse_match <- match(bare_sp, nurse_sp) #the indexes in nurse_sp that correspond to the names in bare_sp
+    
+    #no of species occuring in bare and nurse microsites
+    nboth <- length(nurse_match[which(!is.na(nurse_match))]) 
+    prop_both <- (nboth/length(all_sp)) #proportion of sp that grow in both microsites
+    
+    #number of species that only grow in nurse/bare microsites
+    if (nboth == 0) {
+      nnurse_only = length(nurse_sp)
+      nbare_only = length(bare_sp)
+      
+    }else {
+      nnurse_only <- length(nurse_sp[-nurse_match[which(!is.na(nurse_match))]])
+      nbare_only <- length(bare_sp[-bare_match[which(!is.na(bare_match))]])
+    }
+    
+    prop_nurse <- (nnurse_only/length(all_sp)) #proportion of sp that only grow in nurse microsites
+    
+    prop_bare <- (nbare_only/length(all_sp)) #proportion of sp that only grow in bare microsites
+    
+    
+    pref_cou[l,1] <- plot$SITE_ID[1]
+    pref_cou[l,2] <- plotlist[f]
+    pref_cou[l,3] <- plot$ARIDITY.v3[1] #aridity of the plot
+    pref_cou[l,4] <- plot$GRAZ[1] #grazing pressure of the plot
+    pref_cou[l,5] <- prop_bare
+    pref_cou[l,6] <- prop_nurse
+    pref_cou[l,7] <- prop_both
+    
+    l = l+1
+  }
+  assign(paste("pref", countrynames[i], sep= "_"), pref_cou) #name the dataframe according to the country
+  #Now moving on to the next country to make a separate pref table for that country
+}
+
+#BInd all the pref tables together
+sp_preference <- rbind(pref_algeria, pref_argentina, pref_australia, pref_chile, pref_chinachong, pref_chinaxin, 
+                       pref_iranabedi, pref_iranfarzam, pref_israel, pref_namibiablaum, pref_namibiawang, 
+                       pref_southafrica,  pref_spainmaestre, pref_spainrey)
+#Order ID according to aridity
+sp_preference <- sp_preference[order(sp_preference$aridity),]
+sp_preference$ID <- as.factor(sp_preference$ID)
+sp_preference$site_ID <- as.factor(sp_preference$site_ID)
+sp_preference$graz <- as.factor(sp_preference$graz)
+#Add a quadratic aridity term
+sp_preference$arid_sq <- sp_preference$aridity^2
+
+##Write to a .csv file to use in graphing
+#write.csv(sp_preference, "C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\results\\plotlevels_sp_preference_6Feb2024.csv")
+sp_preference <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\results\\plotlevels_sp_preference_6Feb2024.csv", row.names = 1)
+sp_preference$ID <- as.factor(sp_preference$ID)
+sp_preference$site_ID <- as.factor(sp_preference$site_ID)
+sp_preference$graz <- as.factor(sp_preference$graz)
+
+#In how many of the plots did the majority of sp prefer nurse microsites?
+length(sp_preference[which(sp_preference$prop_nurse_only > sp_preference$prop_both) , ]) #8
+
+#In how many of the sites did the majority of sp prefer open microsites?
+length(sp_preference[which(sp_preference$prop_bare_only > sp_preference$prop_both) , ]) #8
+
+#On average, how many species ocurred in both nurse and bare microsites
+avg_both <- sum(sp_preference$prop_both)/nrow(sp_preference)
+
+
+###Linear modelling: Does the proportion of NURSE only sp change along grazing and aridity?
+##in text the proportion of nurse only species = Pdominant
+
+
+
+
+
 ###DESCRIPTIVE STATISTICS####
 #How many plots and sites
 length(unique(all_result$ID))#97
