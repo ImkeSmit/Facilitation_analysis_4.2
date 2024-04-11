b328e8bb558efc404688a9883e0a9732cb14eb82
ImkeSmit <imke610@gmail.com>
2024-04-11 13:47:23 +0200

    Add chisq test code

--- Scripts/interaction_gradient_models_allsubsets_formulas.R
+++ Scripts/interaction_gradient_models_allsubsets_formulas.R
@@ -390,6 +390,320 @@ for(r in 1:length(response_list)) {
 prefmod_results_table
 
 
+###CHisq tests of species association with nurse or bare microsites####
+###First we need to get the number times a species is present/absent in each microsite
+#Import the country_v3 data
+data_files <- list.files("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3")
+countrynames <- c("algeria", "argentina", "australia", "chile", "chinachong", "chinaxin", "iranabedi", "iranfarzam", 
+                  "israel", "namibiablaum", "namibiawang", "southafrica",  "spainmaestre", "spainrey")
+for(i in 1:length(data_files)) {                              
+  assign(paste0(countrynames[i]),                                   
+         read.csv2(paste0("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\Countriesv3\\",
+                          data_files[i])))
+}
+
+vars <- c("SITE_ID", "ID", "GRAZ", "ARIDITY.v3", "Microsite", "ID_Microsite", "Number.of.replicate", "Species.within.quadrat" )
+data <- rbind(algeria[ , which(colnames(algeria) %in% vars)], argentina[ , which(colnames(argentina) %in% vars)], australia[ , which(colnames(australia) %in% vars)], chile[ , which(colnames(chile) %in% vars)], 
+              chinachong[ , which(colnames(chinachong) %in% vars)], chinaxin[ , which(colnames(chinaxin) %in% vars)], iranabedi[ , which(colnames(iranabedi) %in% vars)], iranfarzam[ , which(colnames(iranfarzam) %in% vars)], 
+              israel[ , which(colnames(israel) %in% vars)], namibiablaum[ , which(colnames(namibiablaum) %in% vars)], namibiawang[ , which(colnames(namibiawang) %in% vars)], southafrica[ , which(colnames(southafrica) %in% vars)],  
+              spainmaestre[ , which(colnames(spainmaestre) %in% vars)], spainrey[ , which(colnames(spainrey) %in% vars)])
+
+column_names <- c("site_ID", "ID", "graz", "aridity", "species", "nurse_p", "nurse_a", "bare_p", "bare_a")
+#nurse_p is the number of nurse microsites in which a species is present
+#nurse_a is the total number of replicates minus the number of nurse presences
+#same goes for the bare microsites
+
+plotlist <- unique(data$ID)
+
+for (f in 1:length(plotlist)) {
+  
+  plot <- data[which(data$ID == plotlist[f]) , ]
+  plot_species <- c(unique(plot$Species.within.quadrat))
+  plot_species <- plot_species[which(!is.na(plot_species))]
+  
+  #subset for each species in a plot
+  for(k in 1:length(plot_species)) {
+    
+    focus_sp <- plot[which(plot$Species.within.quadrat == plot_species[k]) , ]
+    
+    nurse_p <- nrow(focus_sp[which(focus_sp$Microsite == 2) , ])
+    nurse_a <- length(unique(plot$Number.of.replicate)) - nurse_p
+    
+    bare_p <- nrow(focus_sp[which(focus_sp$Microsite == 1) , ])
+    bare_a <- length(unique(plot$Number.of.replicate)) - bare_p
+    
+    if(f+k == 2) { #only make a table for the first result, afterwards we will just rbind to it
+      #create an empty table to put the results in 
+      sp_PrAb <- data.frame(matrix(nrow = 1, ncol = length(column_names)))
+      colnames(sp_PrAb) <- column_names
+      
+      sp_PrAb$site_ID <- plot$SITE_ID[1]
+      sp_PrAb$ID <- plotlist[f]
+      sp_PrAb$graz <- plot$GRAZ[1]
+      sp_PrAb$aridity <- plot$ARIDITY.v3[1]
+      sp_PrAb$species <- plot_species[k]
+      sp_PrAb$nurse_p <- nurse_p
+      sp_PrAb$nurse_a <- nurse_a
+      sp_PrAb$bare_p <- bare_p
+      sp_PrAb$bare_a <- bare_a
+      
+    } else { #now we just rbind to the table we made in the above loop
+      temp <- cbind(plot$SITE_ID[1], plotlist[f], plot$GRAZ[1], plot$ARIDITY.v3[1],  plot_species[k] , nurse_p, nurse_a, 
+                    bare_p, bare_a)
+      colnames(temp) <- column_names
+      sp_PrAb <- rbind(sp_PrAb, temp)
+    }
+  }
+}
+
+
+###MAke a contingency table for each species in sp_PrAb
+
+sp_PrAb$nurse_p <- as.numeric(sp_PrAb$nurse_p)
+sp_PrAb$nurse_a <- as.numeric(sp_PrAb$nurse_a)
+sp_PrAb$bare_a <- as.numeric(sp_PrAb$bare_a)
+sp_PrAb$bare_p <- as.numeric(sp_PrAb$bare_p)
+
+# Create an empty list to store contingency tables
+contingency_tables <- list()
+
+# Iterate through each row in the data frame
+for (i in 1:nrow(sp_PrAb)) {
+  row <- sp_PrAb[i, ]  # Extract the current row
+  
+  # Extract values from the row
+  nurse_p <- row[['nurse_p']]
+  nurse_a <- row[['nurse_a']]
+  bare_p <- row[['bare_p']]
+  bare_a <- row[['bare_a']]
+  
+  # Create a contingency table
+  contingency_table <- matrix(c(nurse_p, nurse_a, bare_p, bare_a), nrow = 2, byrow = TRUE)
+  colnames(contingency_table) <- c('nurse', 'bare')
+  rownames(contingency_table) <- c('p', 'a')
+  
+  # Convert to a table object
+  contingency_table <- as.table(contingency_table)
+  
+  # Add the contingency table to the list
+  contingency_tables[[i]] <- contingency_table
+}
+
+# Now, contingency_tables is a list of contingency tables, one for each row in sp_PrAb, so one for each species and ID combination
+# You can access individual contingency tables using indexing, e.g., contingency_tables[[1]], contingency_tables[[2]], etc.
+
+
+###Now we need to do the Chisquared tests on each contingency table
+# Create an empty data frame to store Chi-squared test results
+Chisq_results <- data.frame(table_no = numeric(0), p_value = numeric(0), exp_nurse_p= numeric(0), exp_nurse_a= numeric(0), 
+                            exp_bare_p= numeric(0), exp_bare_a= numeric(0), res_nurse_p = numeric(0), res_nurse_a = numeric(0), 
+                            res_bare_p = numeric(0), res_bare_a = numeric(0))
+
+# Iterate through each contingency table
+for (i in 1:length(contingency_tables)) {
+  focus_table <- contingency_tables[[i]]
+  
+  # Perform the Chi-squared test
+  chi_squared_result <- chisq.test(focus_table)
+  
+  # Extract the p-value from the test result
+  p_value <- chi_squared_result$p.value
+  
+  #extract the expected results 
+  exp <- chi_squared_result$expected
+  
+  #extract the standard residuals
+  res <- chi_squared_result$residuals
+  
+  # Add the row number, p-value, expected values and std residuals to the data frame
+  Chisq_results <- rbind(Chisq_results, data.frame(table_no = i, p_value = p_value, 
+                                                   exp_nurse_p = exp[1,1], exp_nurse_a = exp[2,1],
+                                                   exp_bare_p = exp[1,2], exp_bare_a = exp[2,2], 
+                                                   res_nurse_p = res[1,1], res_nurse_a = res[2,1],
+                                                   res_bare_p = res[1,2], res_bare_a = res[2,2] ))
+}##There are warnings that it the results may not be accurate??
+#In chisq.test(focus_table) : Chi-squared approximation may be incorrect
+
+#Now we can merge the ID and species names from sp_PrAb to Chisq results
+sp_PrAb$table_no <- rownames(sp_PrAb)
+Chisq_results <- merge(Chisq_results, sp_PrAb, by = "table_no")
+
+###Classify entries whether they have neutral, nurse or bare association, or whether their results are unreliable
+##Add a variable to chisq_filtered that shows whether the species is significantly associated with the nurse/bare microsite or if it shows no association
+Chisq_results$association <- NA
+
+for(i in 1:nrow(Chisq_results)) { 
+  #If any of the expected values is lower than 5, the Chisq approximation may not be correct
+  #These species are too rare to trust their results
+  if(Chisq_results[i , ]$exp_nurse_p <5 | Chisq_results[i , ]$exp_nurse_a <5 | Chisq_results[i , ]$exp_bare_p <5 | Chisq_results[i , ]$exp_bare_a <5) {
+    Chisq_results[i , ]$association <- "too_rare" 
+  } else { 
+    if(Chisq_results[i , ]$p_value > 0.05) { #if p>0.05 there is no significant association to any microsite
+      Chisq_results[i , ]$association <- "neutral"
+    } else {
+      if(Chisq_results[i , ]$res_nurse_p > 0) { #if p<0.05 and res_nurse_p>0 then there is an attraction between the species presence and the nurse microsite
+        Chisq_results[i , ]$association <- "nurse"
+      } else {
+        if(Chisq_results[i , ]$res_bare_p > 0) { #if p<0.05 and res_bare_p>0 then there is an attraction between the species presence and the bare microsite
+          Chisq_results[i , ]$association <- "bare"
+        }
+      }
+    }
+  }
+}
+
+
+#write to csv file
+#write.csv(Chisq_results, "C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\results\\Chisq_results_6Feb2024.csv")
+Chisq_results <- read.csv("C:\\Users\\imke6\\Documents\\Msc Projek\\Facilitation analysis\\Facilitation data\\results\\Chisq_results_6Feb2024.csv", row.names = 1)
+
+#How many sp significantly associated with the nurse?
+Chisq_results |> 
+  filter(association == "nurse") |> 
+  distinct(species) |> 
+  summarise(nsp = n()) #85
+
+#How many sp significantly associated with the bare microsite?
+Chisq_results |> 
+  filter(association == "bare") |> 
+  distinct(species) |> 
+  summarise(nsp = n()) #38
+
+#How many sp show neutral association?
+Chisq_results |> 
+  filter(association == "neutral") |> 
+  distinct(species) |> 
+  summarise(nsp = n()) #241
+
+#How many sp are adequately sampled?
+Chisq_results |> 
+  filter(!association == "too_rare") |> 
+  distinct(species) |> 
+  summarise(nsp = n()) #305
+
+#How many species in total
+Chisq_results |> 
+  select(species) |> 
+  distinct(species) |> 
+  summarise(nsp = n()) #720
+
+####Is species association influenced by aridity and graz?###
+##First, get the proportion of species in a plot that show a certain association
+chisq_reduced <- Chisq_results[-which(Chisq_results$association == "too_rare") , ] #remove the rare sp
+
+prop_chisq_reduced <- chisq_reduced %>%
+  group_by(ID, association) %>%
+  summarize(Count = n()) %>%
+  ungroup() %>%
+  group_by(ID) %>%
+  mutate(Proportion = Count / sum(Count))
+prop_chisq_reduced <- as.data.frame(prop_chisq_reduced)
+prop_chisq_reduced$percentage <- prop_chisq_reduced$Proportion*100
+#Add aridity to prop_chisq
+ymerge <- Chisq_results[-which(duplicated(Chisq_results$ID) == TRUE) , which(colnames(Chisq_results) %in% c("site_ID", "ID", "graz", "aridity"))]
+prop_chisq_reduced <- merge(prop_chisq_reduced, ymerge, 
+                            by = "ID", all.x = FALSE, all.y = FALSE, no.dups = TRUE)
+#add aridity squared
+prop_chisq_reduced$arid_sq <- (prop_chisq_reduced$aridity)^2
+prop_chisq_reduced$site_ID <- as.factor(prop_chisq_reduced$site_ID)
+prop_chisq_reduced$ID <- as.factor(prop_chisq_reduced$ID)
+prop_chisq_reduced$graz <- as.factor(prop_chisq_reduced$graz)
+
+###Now we can make the models
+##PROPORTION OF BARE ASSOCIATIONS##
+baredat <- prop_chisq_reduced[which(prop_chisq_reduced$association == "bare") , ]
+
+nullmod_bare <- glmmTMB(Proportion ~ 1 +(1|site_ID),  
+                        family = binomial, data = baredat)
+
+##graz
+bare_mod1 <- glmmTMB(Proportion ~ graz +(1|site_ID),  
+                     family = binomial, data = baredat)
+summary(bare_mod1)
+Anova(bare_mod1)
+anova(nullmod_bare, bare_mod1) 
+lsmeans(bare_mod1, specs = "graz")
+cld(lsmeans(bare_mod1, specs = "graz"), Letters = "abcdefg")
+
+r.squaredGLMM(bare_mod1) #take the theoretical
+
+##aridity
+bare_mod2 <- glmmTMB(Proportion ~ aridity +(1|site_ID),  
+                     family = binomial, data = baredat)
+
+##aridity + arid_sq
+bare_mod3 <- glmmTMB(Proportion ~ aridity + arid_sq +(1|site_ID),  
+                     family = binomial, data = baredat)
+
+##graz + aridity
+bare_mod4 <- glmmTMB(Proportion ~ graz +aridity + (1|site_ID),  
+                     family = binomial, data = baredat)
+
+##graz + aridity + arid_sq
+bare_mod5 <- glmmTMB(Proportion ~ graz +aridity + arid_sq +(1|site_ID),  
+                     family = binomial, data = baredat)
+
+##graz*aridity
+bare_mod6 <- glmmTMB(Proportion ~ graz*aridity + (1|site_ID),  
+                     family = binomial, data = baredat)
+summary(bare_mod6)
+
+##Graz*aridity + graz*arid_sq
+bare_mod7 <- glmmTMB(Proportion ~ graz*aridity + graz*arid_sq + (1|site_ID),  
+                     family = binomial, data = baredat) 
+
+
+AIC(nullmod_bare, bare_mod1, bare_mod2, bare_mod3, bare_mod4, bare_mod5, bare_mod6, bare_mod7)
+##nullmodel has the lowest AIC
+
+
+
+##PROPORTION OF NURSE ASSOCIATIONS##
+nursedat <- prop_chisq_reduced[which(prop_chisq_reduced$association == "nurse") , ]
+
+nullmod_nurse <- glmmTMB(Proportion ~ 1 +(1|site_ID),  
+                         family = binomial, data = nursedat)
+
+##graz
+nurse_mod1 <- glmmTMB(Proportion ~ graz +(1|site_ID),  
+                      family = binomial, data = nursedat)
+summary(nurse_mod1)
+Anova(nurse_mod1)
+anova(nullmod_nurse, nurse_mod1) 
+lsmeans(nurse_mod1, specs = "graz")
+cld(lsmeans(nurse_mod1, specs = "graz"), Letters = "abcdefg")
+
+r.squaredGLMM(nurse_mod1) #take the theoretical
+
+##aridity
+nurse_mod2 <- glmmTMB(Proportion ~ aridity +(1|site_ID),  
+                      family = binomial, data = nursedat)
+
+##aridity + arid_sq
+nurse_mod3 <- glmmTMB(Proportion ~ aridity + arid_sq +(1|site_ID),  
+                      family = binomial, data = nursedat)
+
+##graz + aridity
+nurse_mod4 <- glmmTMB(Proportion ~ graz +aridity + (1|site_ID),  
+                      family = binomial, data = nursedat)
+
+##graz + aridity + arid_sq
+nurse_mod5 <- glmmTMB(Proportion ~ graz +aridity + arid_sq +(1|site_ID),  
+                      family = binomial, data = nursedat)
+
+##graz*aridity
+nurse_mod6 <- glmmTMB(Proportion ~ graz*aridity + (1|site_ID),  
+                      family = binomial, data = nursedat)
+summary(nurse_mod6)
+
+##Graz*aridity + graz*arid_sq
+nurse_mod7 <- glmmTMB(Proportion ~ graz*aridity + graz*arid_sq + (1|site_ID),  
+                      family = binomial, data = nursedat) ##model convergence problem
+
+
+AIC(nullmod_nurse, nurse_mod1, nurse_mod2, nurse_mod3, nurse_mod4, nurse_mod5, nurse_mod6, nurse_mod7)
+##nullmodel has the lowest AIC
+
 
 
 ###DESCRIPTIVE STATISTICS####
