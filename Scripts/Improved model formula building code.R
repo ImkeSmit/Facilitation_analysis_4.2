1ddd3bcf958d8445abbac76ef00709e35fdb7544
ImkeSmit <imke610@gmail.com>
2024-06-22 10:13:50 +0200

    add the other predictors and paste them together with +

--- Scripts/Improved model formula building code.R
+++ Scripts/Improved model formula building code.R
@@ -1,17 +1,22 @@
-predictors <- c("graz", "aridity", "aridity2", "AMT", "AMT2", "RASE", "pH", "SAC")
+predictors <- c("graz", "aridity", "aridity2", "AMT", "AMT2", "RASE", "pH", "SAC", 
+                "graz:aridity", "graz:RASE", "graz:AMT", "graz:pH", "graz:SAC", "RASE:AMT", "RASE:aridity", "AMT:aridity")
 
+#how many combinations are possible?
+n_possible_models = 2^length(predictors) -1
 
 modlist <- list()
 l = 1
 for(counter1 in 1:length(predictors)) {
   combos <- as.matrix(combn(predictors, counter1))
+  
+  
   for(counter2 in 1:ncol(combos)) {
-    mod <- c(combos[, counter2])
+    mod <- paste(c(combos[, counter2]), collapse = "+")
     
     modlist[[l]] <- mod
     l = l+1
   }
 }
 
-#now loop through every entry in modlist and paste all elements in antry together with + as the separator
+
 
