b78d3070235f4902f5b9ba5015bbc39a6c68725e
ImkeSmit <imke610@gmail.com>
2024-06-22 10:48:42 +0200

    get teh model formulas in this script and then run the models

--- Scripts/interaction_gradient models.R
+++ Scripts/interaction_gradient models.R
@@ -33,7 +33,10 @@ drypop$ID <- as.factor(drypop$ID)
 
 #join the env covariates to the facilitation data
 all_result <- all_result |> 
-  inner_join(drypop, by = "ID")
+  inner_join(drypop, by = "ID") |> 
+  rename(pH = "pH.b", SAC = "SAC.b") |> 
+  mutate(aridity2 = aridity^2, 
+         AMT2 = AMT^2)
 
 #NIntc is bounded beween -1 and 1, so binomial family is appropriate
 #However the function requires that the response be bounded between 0 and 1, so rescale NIntc
@@ -76,13 +79,69 @@ png("nint_correlation.png")
 corrplot(cormat2, method = "number", type = "lower")
 dev.off()
 
-###now run allsubsets to get the model formulas####
+###Get the model formulas####
+predictors <- c("graz", "aridity", "aridity2", "AMT", "AMT2", "RASE", "pH", "SAC", 
+                "graz:aridity", "graz:RASE", "graz:AMT", "graz:pH", "graz:SAC", "RASE:AMT", "RASE:aridity", "AMT:aridity")
 
-###Generalised linear modelling with glmmTMB : NINt ~ AMT + RAI + GRAZ####
-formula_table <- read.csv("Facilitation data\\results\\nint_models_allsubsets_AMT_RAI.csv") |> 
-  separate_wider_delim(formula, delim = "~", names = c("response", "predictors")) |> 
-  select(predictors) |> 
-  distinct(predictors) |> 
+#how many combinations are possible?
+n_possible_models = 2^length(predictors) -1
+
+modlist <- data.frame(formula = character(length = n_possible_models))
+l = 1
+for(counter1 in 1:length(predictors)) {
+  combos <- as.matrix(combn(predictors, counter1))
+  
+  for(counter2 in 1:ncol(combos)) {
+    mod <- paste(c(combos[, counter2]), collapse = "+")
+    
+    modlist[l, 1] <- mod
+    l = l+1
+}}
+
+# Function to check if a model is valid
+is_valid_model <- function(model) {
+  terms <- unlist(strsplit(model, "\\+"))
+  
+  # Define main effects and their corresponding interaction/squared terms
+  interactions <- list("graz" = c("graz:aridity", "graz:RASE", "graz:AMT", "graz:pH", "graz:SAC"),
+                       "aridity" = c("graz:aridity", "RASE:aridity", "AMT:aridity"),
+                       "RASE" = c("graz:RASE", "RASE:AMT", "RASE:aridity"),
+                       "AMT" = c("graz:AMT", "RASE:AMT", "AMT:aridity"),
+                       "pH" = "graz:pH",
+                       "SAC" = "graz:SAC")
+  
+  squared_terms <- list("aridity" = "aridity2", "AMT" = "AMT2")
+  
+  # Check for interaction terms without main effects
+  for (main_effect in names(interactions)) {
+    if (any(interactions[[main_effect]] %in% terms) && !(main_effect %in% terms)) {
+      return(FALSE)
+    }
+  }
+  
+  # Check for squared terms without main effects
+  for (main_effect in names(squared_terms)) {
+    if ((squared_terms[[main_effect]] %in% terms) && !(main_effect %in% terms)) {
+      return(FALSE)
+    }
+  }
+  
+  return(TRUE)
+}
+
+#run modlist through the function to see which models are valid
+validity = c()
+for(m in 1:nrow(modlist)) {
+  validity[m] <- is_valid_model(modlist[m, 1])
+}
+#subset modlist to keep only valid models
+valid_modlist <- data.frame(predictors = modlist[c(which(validity == TRUE)), ])
+write.csv(valid_modlist, "Facilitation data\\results\\model_formulas_22Jul2024.csv")
+
+
+###Generalised linear modelling with glmmTMB : NINt ~ AMT + RASE + aridity + GRAZ####
+formula_table <- read.csv("Facilitation data\\results\\model_formulas_22Jul2024.csv", row.names = 1) |>
+  mutate(predictors = paste(predictors, "(1|site_ID)", sep = "+")) |> #add the random effect to all formulas
   add_row(predictors = "1+(1|site_ID)")  #add the null model
 
 #Create a table for results
