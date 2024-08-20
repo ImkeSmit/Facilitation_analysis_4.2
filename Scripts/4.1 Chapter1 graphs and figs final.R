1e74d94332076aad5d5dce39480b236ef3dc131b
ImkeSmit <imke610@gmail.com>
2024-08-20 13:55:23 +0200

    change this figure to show pH

--- Scripts/4.1 Chapter1 graphs and figs final.R
+++ Scripts/4.1 Chapter1 graphs and figs final.R
@@ -385,25 +385,23 @@ ninta_richness_AMT <- ggplot(all_result, aes(y = NInta_richness, x = AMT)) +
 ggsave("nintA_richness_AMT_scatter.png", ninta_richness_AMT, path = "Figures")
 
 
-###Appendix Fig S4: Scatterplot of NIntA cover over aridity####
+###Appendix Fig S4: Scatterplot of NIntA cover over pH####
 pred_data4 <- all_result |>
-  select(ID, SAC, aridity, AMT, graz, site_ID) |> 
-  distinct() |> 
-  mutate(AMT = mean(AMT), graz = 1, SAC = mean(SAC))
-pred_data4$graz <- as.factor(pred_data4$graz)
+  select(ID, pH) |> 
+  distinct() 
 
-graphmod4 <- glmmTMB(NInta_cover_binom ~ graz+aridity+AMT+SAC+graz:SAC, data = all_result, family = binomial)#remove random effect because otherwise it makes jagged lines
+graphmod4 <- glmmTMB(NInta_cover_binom ~ pH, data = all_result, family = binomial)#remove random effect because otherwise it makes jagged lines
 
 pred_data4$NInta_cover_binom_prediction <- predict(graphmod4, newdata = pred_data4, type = "response") #get nintc_binom predictions
 pred_data4$NInta_cover_prediction <- pred_data4$NInta_cover_binom_prediction*3 -1
 
-ninta_cover_aridity <- ggplot(all_result, aes(y = NInta_cover, x = aridity)) +
-  geom_jitter(height = 0.01, width = 0.01, color = "darkslategrey", alpha = 0.6, size = 1.5) +
-  geom_line(data = pred_data4, aes(x = aridity, y = NInta_cover_prediction), lwd = 1, colour = "darkorange") +
-  labs(y = expression(NInt[A]~cover), x = "Aridity") +
+ninta_cover_pH <- ggplot(all_result, aes(y = NInta_cover, x = pH)) +
+  geom_jitter(height = 0.01, width = 0.01, color = "darkslategrey", alpha = 0.4, size = 1.5) +
+  geom_line(data = pred_data4, aes(x = pH, y = NInta_cover_prediction), lwd = 2, colour = "darkorange") +
+  labs(y = expression(NInt[A]~cover), x = "pH") +
   theme_classic()
 
-ggsave("nintA_cover_aridity_scatter.png", ninta_cover_aridity, path = "Figures", height = 700, width = 800, units = 'px')
+ggsave("nintA_cover_pH_scatter.png", ninta_cover_pH, path = "Figures")
 
 ###Old stuff:Barplot of NINta  cover at different grazing levels####
 #we will do a plot of the arithmetic means, and also the lsmeans with significance letters.
